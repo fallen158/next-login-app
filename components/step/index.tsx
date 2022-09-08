01c0c3601de39d775d456569ed4633f409026eac
lwc <liuwenchao@cqkqinfo.com>
2022-09-08 08:25:35 +0800

    chore: 新增step组件

--- components/step/index.tsx
+++ components/step/index.tsx
@@ -1,30 +1,59 @@
-import classNames from "classnames";
-import Space from "../space";
-import styles from "./index.module.scss";
+import classNames from 'classnames';
+import Space from '../space';
+import styles from './index.module.scss';
 
 interface Props {
   /**
    * 当前的index
    */
   current: number;
-  className?: string;
   /**
    * 子项节点
    */
   items: string[];
+  onChoose?: (i: number) => void;
 }
-const Step = ({ current, className, items }: Props) => {
+const Step = ({ current, items, onChoose }: Props) => {
   return (
-    <Space
-      className={classNames(styles.step, className)}
-      align="center"
-      justify="center"
-    >
-      {items.map((item, index) => (
-        <Space className={styles.item} key={index}>
-          <div className={styles.itemText}>{item}</div>
-        </Space>
-      ))}
+    <Space className={styles.step} align="center" justify="center">
+      {items.map((item, index) => {
+        const active = index <= current - 1;
+        const color = active ? '#1678ff' : '#e3e3e3';
+
+        return (
+          <Space
+            vertical
+            flex='auto'
+            className={classNames(styles.item, active && styles.activeItem)}
+            key={index}
+          >
+            <Space
+              align="center"
+              justify="center"
+              className={styles.numberWrap}
+            >
+              <div
+                className={styles.line}
+                style={{
+                  left:
+                    index === 0
+                      ? '50%'
+                      : index === items.length - 1
+                      ? '-50%'
+                      : 0,
+                  background: current - 1 ? color : '#e3e3e3',
+                }}
+              />
+              <Space align="center" justify="center" className={styles.number}>
+                {index + 1}
+              </Space>
+            </Space>
+            <Space justify="center" className={styles.itemText}>
+              {item}
+            </Space>
+          </Space>
+        );
+      })}
     </Space>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,13 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { Space, Step } from "../components";
-import styles from "../styles/Home.module.css";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { Space, Step } from '../components';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
     <>
-      <Step current={2} items={["ccc", "cccc"]} />
-      {/* <Space ignoreNum={2} size={30}>
-        <div>122888899999100000088888999990000-------99999000000----22</div>
-        <div>2</div>===========22
-        <div>3</div>
-      </Space> */}
+      <Step current={2} items={['身份验证', '修改密码', '完成']} />
     </>
   );
 };
